fc233bdc1554b6d75447d1703b4d72f08c19ee49
Ayush singla <143862828+Ayushsingla1@users.noreply.github.com>
2024-06-04 18:39:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 const cookieparser = require('cookie-parser');
+const cors = require('cors');
 require('dotenv').config();
 app.use(express.json());
 app.use(cookieparser())
+
+app.use(cors(
+    {
+        origin : ["https://food-dev-phi.vercel.app"],
+        methods : ["GET","POST"],
+        credentials : true,
+    }
+    ))
 
 const db = require('./config/database');
 db();
@@ -16,3 +25,5 @@ app.listen(PORT,()=>{
 
 const routes = require('./route/routes')
 app.use('/api/v1',routes);
+
+
