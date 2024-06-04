e8d449ff2527c2e9766454a1906048aafe1ba182
Ayush Singla <143862828+Ayushsingla1@users.noreply.github.com>
2024-06-04 16:57:53 +0530

    first commit

--- config/database.js
+++ config/database.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 const db = ()=>{
-    mongoose.connect(process.env.MONGODB_URL,{
-
+    mongoose.connect(process.env.MONGODB_URI,{
     })
     .then(()=>{})
     .catch((error)=>{
